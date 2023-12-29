6ffc0d8fe88f2b2d6ef4898f260b9db5975deeb1
Oleh Kyrtsun <olegkirtsun@gmail.com>
2023-12-29 17:33:14 +0200

    настройки для github action

--- wordpress_download.sh
+++ wordpress_download.sh
@@ -4,6 +4,10 @@ wordpress_version="6.4.2"
 
 wordpress_url="https://wordpress.org/wordpress-$wordpress_version-ru_RU.zip"
 
+sudo apt-get install unzip
+
+wait
+
 curl -o wordpress.zip https://wordpress.org/wordpress-$wordpress_version.zip
 
 wait
